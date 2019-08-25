95fb981da1b82d00c587046e3c33b135de4f2bc5
rizworks <idobataog@gmail.com>
2019-08-25 16:12:59 +0900

    メソッド追加

--- apigw_ui/src/components/top.vue
+++ apigw_ui/src/components/top.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
     <h1>ここがすべての始まりだ</h1>
-    <img src="static/image/hajimari.jpeg"/>
+    <div>
+      <img src="static/image/hajimari.jpeg" style="width:120px"/>
+    </div>
+    <div id="ex1">
+      <button v-on:click="getList">API Gatewayにアクセスする</button>
+    </div>
     <h2>{{first_msg}}</h2>
   </div>
 </template>
 
 <script>
+
 export default {
   data () {
     return {
-      first_msg : 'はじめまして"はじまり"です。',
+      first_msg: 'はじめまして"はじまり"です。'
+    }
+  },
+  methods: {
+    getList () {
+      console.log('xxx')
     }
   }
 }
+
 </script>
