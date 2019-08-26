f57d7a413d53bc7ff9ed4c79a94d543ea0ec959c
rizworks <idobataog@gmail.com>
2019-08-26 09:09:51 +0900

    Code refactor

--- apigw_ui/src/components/top.vue
+++ apigw_ui/src/components/top.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-
+var AWS = require('aws-sdk');
 export default {
   data () {
     return {
@@ -21,6 +21,17 @@ export default {
   },
   methods: {
     getList () {
+      var apigateway = new AWS.APIGateway({apiVersion: '2015-07-09'});
+      var credentials = new AWS.SharedIniFileCredentials({profile: 'apigw-ui-account'});
+      AWS.config.credentials = credentials;
+      var params = {
+        resourceId: 'ijjcnlkghg',
+        restApiId: 'wiyn9yxq5a'
+      };
+      apigateway.getResource(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data);
+      });
       console.log('xxx')
     }
   }
